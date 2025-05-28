2aad4fecdd755ee1235374fbdaaaa10262487800
Chanjin Park <97965467+Chanjin926@users.noreply.github.com>
2025-05-28 16:54:51 +0900

    Update array_can.py

--- examples/motionplanning/panda/solutions/array_can.py
+++ examples/motionplanning/panda/solutions/array_can.py
@@ -31,6 +31,7 @@ def move_with_rotation_retries(planner, pose: sapien.Pose, max_retries=12):
 def pick_place_with_obstacles(
     env,
     planner: PandaArmMotionPlanningSolver,
+    index: int,
     pick: list[float] | np.ndarray,
     place: list[float] | np.ndarray,
     all_cans: list[list[float]],
@@ -72,7 +73,9 @@ def pick_place_with_obstacles(
     move_with_rotation_retries(planner, place_pose)
     planner.open_gripper()
     move_with_rotation_retries(planner, above_pose)
-
+    
+    all_cans[index] = place
+    return all_cans
 
 def solve(env: ArrayCanEnv, seed=None, debug=False, vis=False):
     env.reset(seed=seed)
@@ -83,24 +86,35 @@ def solve(env: ArrayCanEnv, seed=None, debug=False, vis=False):
         print_env_info=False,
     )
 
-    # 모든 캔 위치: LLM으로 나중에 대체
+    # 모든 캔 위치 (나중에 VLM으로 대체)
+    # seed = 0
     pick = [[0.11796,  0.056426, 0.08],
             [0.0148988,0.205559, 0.08],
             [-0.0594433,0.199338,0.08],
             [-0.0628068,0.0752942,0.08],
             [-0.12443, -0.0406532,0.08]]
+    # # seed = 1
+    # pick = ([0.066385, 0.0737458, 0.08], 
+    #         [0.037905, 0.0130262, 0.08], 
+    #         [0.143296, -0.0365266, 0.08], 
+    #         [0.121768, -0.115834, 0.08], 
+    #         [0.0418419, -0.12208, 0.08])
+    # # seed = 2
+    # pick = ([-0.00316405, 0.0917179, 0.08], 
+    #         [-0.0914487, 0.0033105, 0.08], 
+    #         [0.0155603, 0.00856102, 0.08], 
+    #         [-0.058669, -0.0886018, 0.08], 
+    #         [0.070955, -0.0385983, 0.08])
 
     place = [[-0.24,0.28,0.08],
              [-0.24,0.18,0.08],
              [-0.24,0.08,0.08],
              [-0.24,-0.02,0.08],
              [-0.24,-0.12,0.08]]
+    
+    for i in range(env.num_cans):
+        pick = pick_place_with_obstacles(env.unwrapped, solver, i, pick[i], place[i], pick)
 
-    pick_place_with_obstacles(env.unwrapped, solver, pick[0], place[0], pick)
-    pick_place_with_obstacles(env.unwrapped, solver, pick[1], place[1], pick)
-    pick_place_with_obstacles(env.unwrapped, solver, pick[2], place[2], pick)
-    pick_place_with_obstacles(env.unwrapped, solver, pick[3], place[3], pick)
-    pick_place_with_obstacles(env.unwrapped, solver, pick[4], place[4], pick)
 
     solver.close()
     return True
